68d2f0a2302e706aa74d120479ac5126fc7e9930
Zeref <krishh16@yahoo.com>
2024-10-15 22:11:20 +0530

    added navbar to the app 3

--- app/[selectedOption]/page.tsx
+++ app/[selectedOption]/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { useParams } from "next/navigation";
-import {Gift, CheckCircle, Disc, Flame, Wallet} from "lucide-react"
-import {useState} from "react"
 
 export default function SelectedOptionPage() {
     const params = useParams();
     const selectedOption = params.selectedOption;
-    const [activeTab, setActiveTab] = useState("Tap")
 
     return (
      <div>
